af0087fe8c70db282d304df72b83eaef29ee3a7e
soniascr <soniascr@gmail.com>
2021-02-27 20:41:23 +0000

    first commit

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,103 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    firstname = db.Column(db.String(120), unique=True, nullable=False)
+    lastname = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    posts = db.relationship('Post', backref='user', lazy=True)
+    comments = db.relationship('Comment', backref='user', lazy=True)
+    #followers = db.relationship('Follower', backref='user', lazy=True)
+    
     def __repr__(self):
         return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    def get_all_users():
+        return User.query.all()
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    medias = db.relationship('Media', backref='post', lazy=True)
+    comments = db.relationship('Comment', backref='post', lazy=True)
+    
+    def __repr__(self):
+        return '<Post %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            # do not serialize the password, its a security breach
+        }
+
+    def get_all_posts():
+         return Post.query.all()
+
+class Media(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    kind = db.Column(db.String(120), unique=True, nullable=False)
+    url = db.Column(db.String(80), unique=False, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Media %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "url": self.url
+            # do not serialize the password, its a security breach
+        }
+
+    def get_all_medias():
+         return Media.query.all()
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment_text = db.Column(db.String(300), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Comment %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment_text": self.comment_text
+            # do not serialize the password, its a security breach
+        }
+
+    def get_all_comments():
+         return Comment.query.all()
+
+    
+# class Follower(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_from_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     user_to_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+#     def __repr__(self):
+#         return '<Follower %r>' % self.id
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             # do not serialize the password, its a security breach
+#         }
+
+#     def get_all_followers():
+#          return Follower.query.all()
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Post, Media, Comment #Follower
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,43 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/users', methods=['GET'])
+def handle_users():
+
+    users = User.get_all_users()
+    all_users = list(map(lambda user: user.serialize(), users))
+
+    return jsonify(all_users), 200
+
+@api.route('/posts', methods=['GET'])
+def handle_posts():
+
+    posts = Post.get_all_posts()
+    all_posts = list(map(lambda post: post.serialize(), posts))
+
+    return jsonify(all_posts), 200
+
+@api.route('/medias', methods=['GET'])
+def handle_medias():
+
+    medias = Media.get_all_medias()
+    all_medias = list(map(lambda media: media.serialize(), medias))
+
+    return jsonify(all_medias), 200
+
+@api.route('/comments', methods=['GET'])
+def handle_comments():
+
+    comments = Comment.get_all_comments()
+    all_comments = list(map(lambda comment: comment.serialize(), comments))
+
+    return jsonify(all_comments), 200
+
+# @api.route('/followers', methods=['GET'])
+# def handle_followers():
+
+#     followers = Follower.get_all_followers()
+#     all_followers = list(map(lambda follower: follower.serialize(), followers))
+
+#     return jsonify(all_followers), 200
